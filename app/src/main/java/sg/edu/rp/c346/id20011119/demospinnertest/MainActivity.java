52d9930a014b6bdfd8cb7964b3a0eb536c3cb25f
zhaoyubot <20011119@myrp.edu.sg>
2022-06-02 14:21:19 +0800

    Lesson 7 Worksheet Activity 2 Demo Spinner Test

---
+++ app/src/main/java/sg/edu/rp/c346/id20011119/demospinnertest/MainActivity.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.c346.id20011119.demospinnertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spnYesNo;
+    TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spnYesNo = findViewById(R.id.spinner);
+        tvDisplay = findViewById(R.id.textView);
+
+        spnYesNo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        tvDisplay.setText("Spinner Item Yes Selected");
+                        break;
+                    case 1:
+                        tvDisplay.setText("Spinner Item No Selected");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
